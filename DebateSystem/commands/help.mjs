8a9f333ecec9798541d4fa9fa2d19160087c11d2
Akins <82357502+Akins2229@users.noreply.github.com>
2021-10-06 08:22:16 -0700

    Update help.mjs

--- DebateSystem/commands/help.mjs
+++ DebateSystem/commands/help.mjs
@@ -1,11 +1,33 @@
 import { Command } from "./commandHandler/index.mjs";
+const { MessageEmbed } = require("discord.js");
 
 class Help extends Command {
-  constructor(bot) {
-    super("help", "Returns a list of commands.", "Returns a list of commands and descriptions.", "<command> optional.");
+  constructor(message, bot) {
+    this.message = message;
+    this.bot=bot;
+    super("help", "Returns a list of commands.", "Returns a list of commands and descriptions.", "<command> optional.", "Help");
   }
   
-  exec() {
-    return True
+  exec(command) {
+    
+    embeds = [];
+    
+    bot.category.keys().forEach(key => {
+      const embed = new MessageEmbed()
+      .setTitle("Help Command - " + key)
+      .setDescription(bot.category[key]['description'])
+      .setColor("#32a852");
+      bot.category[key]['commands'].forEach(command => {
+        embed.addField(command, bot.category[key]['commands']['brief']);
+      })
+      
+      embeds.push(embed);
+    })
+    
+    this.channel.send(
+      {
+      embeds: embeds
+      }
+    );
   }
 }
