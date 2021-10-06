d865b159252a03d68a38fc01f9e3e3ae2a037dd0
Akins <82357502+Akins2229@users.noreply.github.com>
2021-10-06 07:11:22 -0700

    Create index.js

---
+++ DebateSystem/index.js
@@ -0,0 +1,28 @@
+import { Bot } from "./commandHandler/index.mjs";
+
+import { Help } from "./commands/help.mjs";
+
+bot = Bot(
+  "$",
+  "A discord debate system.",
+  "Wilson",
+  true
+);
+
+//load commands
+
+commands = [
+  Help(bot);
+];
+
+commands.forEach(command => {
+  bot.addCommand(command);
+})
+
+bot.once('ready', () => {
+  console.log("Wilson Debate System Online!");
+})
+
+bot.on('message', (message) => {
+  bot.parseCommands(message)
+})
